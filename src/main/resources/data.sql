fc3e72e6a8de224b028adf0a5a951f20c80bfd5d
David-Inkeart <okoliedave1@gmail.com>
2024-04-11 11:16:25 +0100

    update code and sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO role (name) VALUES('ROLE_USER_ADMIN');
-INSERT INTO users (email, role_fk) VALUES('nwajeigoddowell@gmail.com', 1);
-
+-- INSERT INTO role (name) VALUES('ROLE_USER_ADMIN');
+-- INSERT INTO users (email, role_fk) VALUES('nwajeigoddowell@gmail.com', 1);-- Check if the role exists
+-- Insert or update the role
+INSERT INTO role (name)
+VALUES ('ROLE_USER_ADMIN') ON CONFLICT (name) DO NOTHING;
+-- Insert or update the user
+INSERT INTO users (email, role_fk)
+VALUES ('nwajeigoddowell@gmail.com', 1) ON CONFLICT (email) DO NOTHING;
