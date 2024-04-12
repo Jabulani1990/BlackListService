129655b3cf5aeb481740eb4e9e1b41eeb0e890cb
Oyewale Abdulganiyu <olawaleabdulganiyujabulani@gmail.com>
2024-04-12 13:32:49 +0100

    CORS

--- src/main/java/Blacklist/Manager/configuration/CorsConfig.java
+++ src/main/java/Blacklist/Manager/configuration/CorsConfig.java
@@ -1,23 +1,53 @@
 package Blacklist.Manager.configuration;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
 
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // Define CORS policy details
-        registry.addMapping("/**")  // Apply CORS to all routes
-                .allowedOrigins("*")  // Allow this origin, use "*" for all if needed
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Specify allowed methods
-                .allowedHeaders("*")  // Allow all headers
-                .allowCredentials(true)  // Allow credentials such as cookies, authorization headers, etc.
-                .maxAge(3600);  // How long the response from the pre-flight request can be cached
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import io.swagger.v3.oas.models.PathItem.HttpMethod;
+import lombok.RequiredArgsConstructor;
+
+
+
+@Component
+@WebFilter("/*")
+@RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfig extends OncePerRequestFilter {
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                     FilterChain filterChain) throws ServletException, IOException
+    {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Method", "GET,POST,OPTIONS,DELETE");
+        response.setHeader("Access-Control-Allow-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Expose-Headers", "Authorization, accept, Content-Type, origin");
+
+        if(request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.name())){
+            response.setStatus(HttpServletResponse.SC_OK);
+            return ;
+        }
+
+        filterChain.doFilter(request, response);
+
+        
+
     }
+
+
     
 }
