2a2a786f10fa380ec4c2d0475874a49c45295563
David-Inkeart <okoliedave1@gmail.com>
2024-04-16 20:36:44 +0100

    update hibernate strategy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,4 @@
---  INSERT INTO role (name) VALUES('ROLE_USER_ADMIN');
--- INSERT INTO users (email, role_fk) VALUES('nwajeigoddowell@gmail.com', 1);-- Check if the role exists
--- Insert or update the role
 INSERT INTO role (name)
 VALUES ('ROLE_USER_ADMIN') ON CONFLICT (name) DO NOTHING;
--- Insert or update the user
 INSERT INTO users (email, role_fk)
-VALUES ('nwajeigoddowell@gmail.com', 1) ON CONFLICT (email) DO NOTHING;
+VALUES ('reiymondjon134@gmail.com', 1) ON CONFLICT (email) DO NOTHING;
